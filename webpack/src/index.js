f6104222a54e7f21e1ddb4dd9a1266bccc83bf81
Bayram Soltanov <bayramsoltanov01@gmail.com>
2023-05-27 23:41:45 +0300

    babel

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,8 +3,13 @@ import "./index.scss";
 
 document.getElementById("btnSayHello").addEventListener("click",()=>{
     alert("Hi, study hard!");
+    
+    const arr = [123,56,893,23,65,87,3];
+    console.log(getArray([...arr]));
 
     setItem("token","34762001-22S423-3423-222-190733498-23");
 })
 
-
+const getArray = (arr)=> {
+    return arr.filter( (x)=>x>100).reduce( (t,x)=> t+x, 0);
+}
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,30 +1,49 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-const {merge}=require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-
-
 const prod = {
   mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "./js/[name].[contenthash].js",
-    clean:true,
+    clean: true,
     assetModuleFilename: "img/[hash][ext]"
-  },
-  module:{
+  }, 
+  module: {
     rules:[
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [
+            MiniCssExtractPlugin.loader, 
+            "css-loader"
+        ],
       },
-
       {
         test: /\.s(a|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+            MiniCssExtractPlugin.loader, 
+            "css-loader", 
+            "sass-loader"
+        ],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: 
+              ['@babel/preset-env']
+          }
+        }
+      }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].[contenthash].css",
+    })
+  ]
 };
-
-module.exports = merge(common,prod);
+module.exports = merge(common, prod);
